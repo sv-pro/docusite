ef91e3efa142ad1a65d9c0a306638d2e1b46f6d5
Sergey Vlasov <sergeyv@radware.com>
2022-10-17 12:17:24 +0300

    gh-pages deployment fixed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,17 +8,19 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  //url: 'https://your-docusaurus-test-site.com',
+  url: 'https://sv-pro.github.io',
+  baseUrl: '/docusite/',
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'sv-pro', // Usually your GitHub org/user name.
+  projectName: 'docusite', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
